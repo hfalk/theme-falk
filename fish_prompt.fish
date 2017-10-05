e2d34afb1fc04ea9cc06835d5c5aa3cd98368435
Håvard Falk <hfalk4@gmail.com>
2017-10-05 20:03:48 +0200

    Initial commit

---
+++ fish_prompt.fish
@@ -0,0 +1,81 @@
+# Print current user
+function prompt_get_user -d "Print the user"
+  if test $USER = 'root'
+    set_color red
+  else
+    set_color cyan
+  end
+  printf '%s' (whoami)
+end
+
+# Get Machines Hostname
+function prompt_get_host -d "Get Hostname"
+  if test $SSH_TTY
+    tput bold
+    set_color red
+  else
+    set_color yellow
+  end
+  printf '%s' (hostname|cut -d . -f 1)
+end
+
+# Get Project Working Directory
+function prompt_pwd -d "Get PWD"
+  set_color $fish_color_cwd
+  printf '%s ' (prompt_pwd)
+end
+
+# Set GIT prompt
+function prompt_git -d "Get GIT status"
+  # Just calculate these once, to save a few cycles when displaying the prompt
+  if not set -q __fish_prompt_hostname
+    set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+  end
+  if not set -q __fish_prompt_char
+    switch (id -u)
+      case 0
+        set -g __fish_prompt_char \u276f\u276f
+      case '*'
+        set -g __fish_prompt_char »
+    end
+  end
+
+  # Configure __fish_git_prompt
+  set -g __fish_git_prompt_show_informative_status true
+  set -g __fish_git_prompt_showcolorhints true
+
+  printf '(%s)' (__fish_git_prompt)
+
+function fish_prompt
+  set -l code $status
+
+  # Logged in user
+  prompt_get_user
+  set_color normal
+  printf ' at '
+
+  # Machine logged in to
+  prompt_get_host
+  set_color normal
+  printf ' in '
+
+  # Path
+  prompt_pwd
+  set_color normal
+
+  # Git info
+  prompt_git
+
+  # Line 2
+  echo
+  if test $VIRTUAL_ENV
+    printf "(python:%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
+  end
+
+  if test $code -eq 127
+    set_color red
+  end
+
+  printf '↪ '
+  set_color normal
+end
---
+++ fish_right_prompt.fish
@@ -0,0 +1,20 @@
+function tbytes -d 'Calculates the total size of the files in the current directory'
+  set -l tBytes (ls -al | grep "^-" | awk 'BEGIN {i=0} { i += $5 } END { print i }')
+
+  if test $tBytes -lt 1048576
+    set -g total (echo -e "scale=3 \n$tBytes/1048 \nquit" | bc)
+    set -g units " Kb"
+  else
+    set -g total (echo -e "scale=3 \n$tBytes/1048576 \nquit" | bc)
+    set -g units " Mb"
+  end
+  echo -n "$total$units"
+end
+
+function fish_right_prompt -d "Write out the right prompt"
+  set_color blue
+  printf '%s' (tbytes)
+  # Timestamp
+  set_color 555
+  printf '%s' (date "+%H:%M:%S")
+end
