38404570ba46a35f74f4ac1073c8a2d25a94ed92
Håvard Falk <hfalk4@gmail.com>
2017-10-05 22:29:07 +0200

    Test

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -52,9 +52,7 @@ end
 
 function fish_prompt
   set -l last_status $status
-  set_color $fish_color_autosuggestion ^/dev/null; or set_color 555
-  date "+%H:%M:%S"
-  set_color normal
+  
   # Logged in user
   prompt_get_user
   printf ' at '
