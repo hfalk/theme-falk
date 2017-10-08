b63c16d3b54e68ed3b6aa2cec6988173897aba85
Håvard Falk <hfalk@users.noreply.github.com>
2017-10-08 14:56:43 +0200

    Update fish_right_prompt.fish

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,6 +1,5 @@
 function fish_right_prompt
-  #set_color $fish_color_autosuggestion ^/dev/null; or set_color 555
-  #date "+%H:%M:%S"
-  printf (dim)(date +%H(fst):(dim)%M(fst):(dim)%S)(off)" "
+  set_color $fish_color_autosuggestion ^/dev/null; or set_color 555
+  date "(+%H:%M:%S)"
   set_color normal
 end
