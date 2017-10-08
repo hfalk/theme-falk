80b518b25b49f1bf1b9e98c9bb08f5e96c0f9635
Håvard Falk <hfalk@users.noreply.github.com>
2017-10-08 14:58:55 +0200

    Update fish_right_prompt.fish

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,5 +1,6 @@
 function fish_right_prompt
-  set_color $fish_color_autosuggestion ^/dev/null; or set_color 555
+  set_color blue
+  #set_color $fish_color_autosuggestion ^/dev/null; or set_color 555
   printf '[%s]' (date "+%H:%M:%S")
   set_color normal
 end
